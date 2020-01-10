c9404d34e8bd7039ec3a661aec921402bcc67b6c
abc <abc@example.com>
2020-01-10 15:07:22 +0100

    first commit

---
+++ src/main/java/com/telixia/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,17 @@
+package com.telixia.microservice.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;  
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.telixia.microservice.currencyconversionservice") 
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
